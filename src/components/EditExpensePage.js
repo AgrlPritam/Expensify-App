04f59683372556f26e7362504e55a07322ca59b9
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-09-06 20:40:57 +0530

    Minor Changes

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const EditExpensePage = () => (
+const EditExpensePage = (props) => {
+    return (
     <div>
-        This is from Edit Expense Component
+        Editing expense with id of {props.match.params.id}
     </div>
-)
+)}
 
 export default EditExpensePage;
