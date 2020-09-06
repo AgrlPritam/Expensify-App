0e246e302dbd022af492f3d0c119db6361cfcbdd
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-09-06 18:31:42 +0530

    Breaking into Components for each route

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+import React from 'react'
+import  Header from '../components/Header';
+import  EditExpense from '../components/EditExpensePage';
+import  Dashboard from '../components/ExpenseDashboardPage';
+import  AddExpense from '../components/AddExpensePage';
+import  HelpPage from '../components/HelpPage';
+import  NotFoundPage from '../components/NotFoundPage';
+
+const AppRouter = () => (
+    <BrowserRouter>
+    <div>
+        <Header />
+        <Switch>
+            <Route path="/" component={Dashboard} exact={true} />
+            <Route path="/create" component={AddExpense} />
+            <Route path="/edit" component={EditExpense} />
+            <Route path="/help" component={HelpPage} />
+            <Route component={NotFoundPage}/>
+        </Switch> 
+    </div>       
+    </BrowserRouter>
+)
+
+export default AppRouter;
