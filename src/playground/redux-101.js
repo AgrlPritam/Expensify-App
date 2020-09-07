a699e00d4174b03103b0e191c08999dbb9e3108e
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-09-07 13:31:06 +0530

    Action Generators using Array Destructuring in Redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,16 +1,36 @@
 import { createStore} from 'redux'
 
+//Action Generators -> functions that return action objects
+const incrementCount = ({ incrementBy = 1 } = {}) => ({       //Array Destructuring
+        type: 'INCREMENT',
+        incrementBy
+})
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+})
+
+const resetCount = () => ({
+    type: 'RESET'
+})
+
+const setCount = ({ count }) => ({
+    type: 'SET',
+    count
+})
+
 const store = createStore((state = { count:0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            const incrementBy = typeof action.incrementBy==='number' ? action.incrementBy : 1;
+            //const incrementBy = typeof action.incrementBy==='number' ? action.incrementBy : 1;
             return {
-                count : state.count + incrementBy
+                count : state.count + action.incrementBy
             }   
         case 'DECREMENT':
             const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count : state.count - decrementBy
+                count : state.count - action.decrementBy
             }   
         case 'RESET':
             return {
@@ -30,31 +50,43 @@ const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
 })
 
-store.dispatch({
-    type: 'INCREMENT',      //type is a required field in dispatch 
-    incrementBy: 5
-})
+store.dispatch(incrementCount())
 
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 10
-})
+store.dispatch(incrementCount({ incrementBy: 5 }))
 
-store.dispatch({
-    type: 'INCREMENT',
-})
+store.dispatch(decrementCount())
+
+store.dispatch(decrementCount({decrementBy:10}))
+
+store.dispatch(resetCount())
+
+store.dispatch(setCount({ count:101 }))
+
+// store.dispatch({
+//     type: 'INCREMENT',      //type is a required field in dispatch 
+//     incrementBy: 5
+// })
+
+// store.dispatch({
+//     type: 'DECREMENT',
+//     decrementBy: 10
+// })
+
+// store.dispatch({
+//     type: 'INCREMENT',
+// })
 
 //unsubscribe()   //This unsubscribe from running the function inside store.subscribe
 
-store.dispatch({
-    type: 'RESET'
-})
+// store.dispatch({
+//     type: 'RESET'
+// })
 
-store.dispatch({
-    type: 'DECREMENT'
-})
+// store.dispatch({
+//     type: 'DECREMENT'
+// })
 
-store.dispatch({
-    type: 'SET',
-    count: 101
-})
+// store.dispatch({
+//     type: 'SET',
+//     count: 101
+// })
