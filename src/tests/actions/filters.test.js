5716afc4ae76e442b8ecc5c4c17969468ae174ee
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-09-11 10:28:16 +0530

    Action Filters test cases added

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,42 @@
+import {setStartDate, setEndDate,setTextFilter,sortByAmount,sortByDate} from '../../actions/filters'
+import moment from 'moment'
+
+test('Should generate set start date action object',() => {
+    const action = setStartDate(moment(0))
+    expect(action).toEqual({
+        type: 'SET_START_DATE',
+        startDate: moment(0)
+    })
+})
+
+test('Should generate set end date action object',() => {
+    const action = setEndDate(moment(0))
+    expect(action).toEqual({
+        type: 'SET_END_DATE',
+        endDate: moment(0)
+    })
+})
+
+test('Should generate sortByAmount action object',() => {
+    expect(sortByAmount()).toEqual({type: 'SORT_BY_AMOUNT'})
+})
+
+test('Should generate sortByDate action object',() => {
+    expect(sortByDate()).toEqual({type: 'SORT_BY_DATE'})
+})
+
+test('Should generate text filters without parameters action object',() => {
+    const action = setTextFilter()
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text: ''
+    })
+})
+
+test('Should generate text filters with parameters action object',() => {
+    const action = setTextFilter('Something')
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text: 'Something'
+    })
+})
--- src/tests/add.test.js
+++
@@ -1,6 +0,0 @@
-const add = (a,b) => a+b;
-
-test('Should Add 2 numbers',()=>{
-    const result = add(3,4)
-    expect(result).toBe(7)
-})
