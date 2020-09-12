786f98dff21eb368ec99f5600c342f57744225d5
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-09-12 15:49:12 +0530

    Changing AddExpensePage from stateless to class based for easy testing of dependencies

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,16 +3,40 @@ import {connect} from 'react-redux'
 import ExpenseForm from './ExpenseForm'
 import {addExpense} from '../actions/expenses'
 
-const AddExpensePage = (props) => (
-    <div>
-        <h1>Add Expense</h1>
-        <ExpenseForm 
-            onSubmit={(expense) => {
-                props.dispatch(addExpense(expense))
-                props.history.push('/')     //This is taken from react components from browser from addExpense--> history-->push function
-            }}
-        />
-    </div>
-)
+export class AddExpensePage extends React.Component {
+    onSubmit = (expense) => {
+        this.props.onSubmit(expense)                 
+        this.props.history.push('/')      //This is taken from react components from browser from addExpense--> history-->push function
+    }
+    render() {
+        return (
+            <div>
+                <h1>Add Expense</h1>
+                <ExpenseForm 
+                    onSubmit={this.onSubmit}
+                />
+            </div>
+        )
+    }
+}
 
-export default connect()(AddExpensePage);
+//This function is added for making test cases easy
+const mapDispatchToProps = (dispatch) => ({         //check react-redux docs
+    onSubmit: (expense) => dispatch(addExpense(expense))
+})
+
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
+
+// const AddExpensePage = (props) => (
+//     <div>
+//         <h1>Add Expense</h1>
+//         <ExpenseForm 
+//             onSubmit={(expense) => {
+//                 //props.dispatch(addExpense(expense))
+//                 props.onSubmit(expense)                 //Added for making testing easy
+//                 props.history.push('/')    
+//             }}
+//         />
+//     </div>
+// )
+
