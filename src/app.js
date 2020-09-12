9168f552c9af34e2d28f8a3cc278325dff93df37
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-09-12 19:51:18 +0530

    Removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -3,25 +3,25 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
-import {addExpense} from './actions/expenses'
-import {setTextFilter} from './actions/filters'
-import {getVisibleExpenses} from './selectors/expenses'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
+// import {addExpense} from './actions/expenses'
+// import {setTextFilter} from './actions/filters'
+// import {getVisibleExpenses} from './selectors/expenses'
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water Bill', amount:400 }))
-store.dispatch(addExpense({description: 'Gas Bill', createdAt:15522}))
-store.dispatch(addExpense({description: 'Rent', amount:3000, createdAt: 12255}))
-store.dispatch(setTextFilter('water'))
+// store.dispatch(addExpense({description: 'Water Bill', amount:400 }))
+// store.dispatch(addExpense({description: 'Gas Bill', createdAt:15522}))
+// store.dispatch(addExpense({description: 'Rent', amount:3000, createdAt: 12255}))
+// store.dispatch(setTextFilter('water'))
 
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses,state.filters)
-console.log(visibleExpenses);
+// const state = store.getState()
+// const visibleExpenses = getVisibleExpenses(state.expenses,state.filters)
+// console.log(visibleExpenses);
 
-console.log(store.getState())
+// console.log(store.getState())
 
 const jsx = (
     <Provider store={store}>
