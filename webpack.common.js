4323b3ff138c3184856aa16585ba8f6888bac15f
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-09-14 18:59:04 +0530

    Segregating Webpack for Prod and Dev Environment

--- webpack.common.js
+++ webpack.common.js
@@ -23,11 +23,5 @@ module.exports = {
                 'sass-loader'
             ]
         }]
-    },
-    devtool: 'eval-cheap-module-source-map',
-    devServer: {         //When we use devServer, we can delete bundle.js and the appl will run fine as it is now handled by webpack dev server which serves the appl from memory. to rebuild the bundle.js we need to run yarn run webpack and bundle.js will be re-created
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    },
-
+    }
 }
