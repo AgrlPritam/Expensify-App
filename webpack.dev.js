07f658d1ec0206412fd0ae0bde5251e160f44053
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-09-15 20:01:25 +0530

    Resetting for deployement

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,7 +7,8 @@ module.exports = merge(common, {
     devtool: '@inline-source-map',
     devServer: {         //When we use devServer, we can delete bundle.js and the appl will run fine as it is now handled by webpack dev server which serves the appl from memory. to rebuild the bundle.js we need to run yarn run webpack and bundle.js will be re-created
         contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+        historyApiFallback: true,
+        publicPath: '/dist/'
     },
 
 })
