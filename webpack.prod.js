01e8b7a77dee9cfff3a8297d5edada1103b143e4
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-09-15 19:41:19 +0530

    Removing Unwanted Bundle files

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,9 +4,5 @@ const { merge } = require('webpack-merge');
 
  module.exports = merge(common, {
    mode: 'production',
-   devtool: '#source-map',
-   devServer: {         //When we use devServer, we can delete bundle.js and the appl will run fine as it is now handled by webpack dev server which serves the appl from memory. to rebuild the bundle.js we need to run yarn run webpack and bundle.js will be re-created
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-},
+   devtool: '#source-map'
  });
