3db2213df3926057f942929edf7f3954114a219d
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-09-20 20:30:25 +0530

    Adding plugin to setup env variables for firebase credentials

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,15 @@
 //entry --> output
 const path = require('path')
+const webpack = require('webpack')
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+if(process.env.NODE_ENV === 'test') {
+    require('dotenv').config({path: './.env.test'})
+} else if(process.env.NODE_ENV === 'development'){
+    require('dotenv').config({path: './.env.development'})
+}
 
 module.exports = {
     entry:'./src/app.js',
@@ -33,6 +42,17 @@ module.exports = {
         }]
     },
     plugins: [new MiniCSSExtractPlugin({
-        filename: 'styles.css'
-    })]
-} 
+            filename: 'styles.css'
+        }),
+        new webpack.DefinePlugin({
+            'process.env.APIKEY': JSON.stringify(process.env.APIKEY),
+            'process.env.AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
+            'process.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
+            'process.env.PROJECT_ID': JSON.stringify(process.env.PROJECT_ID),
+            'process.env.STORAGE_BUCKET': JSON.stringify(process.env.STORAGE_BUCKET),
+            'process.env.MESSAGING_SENDER_ID': JSON.stringify(process.env.MESSAGING_SENDER_ID),
+            'process.env.APP_ID': JSON.stringify(process.env.APP_ID),
+            'process.env.MEASUREMENT_ID': JSON.stringify(process.env.MEASUREMENT_ID)
+        })
+    ]
+}
