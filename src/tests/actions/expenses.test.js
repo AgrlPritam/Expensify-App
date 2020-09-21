3989c1dc523decb494a08684fcc856a847bf6a64
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-09-21 11:45:09 +0530

    Testing edit expense to database

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk'
 import { startAddExpense,
      addExpense, 
      editExpense, 
+     startEditExpense,
      removeExpense, 
      startRemoveExpense, 
      setExpenses, 
@@ -53,6 +54,24 @@ test('Should Setup Edit expense Action Object',() => {
         updates: {
             note: 'New Note'
         }
+    })
+})
+
+test('Should edit expense from firebase', (done) => {
+    const store = createMockStore({})
+    const id = expenses[0].id
+    const updates = { amount: 568 }
+    store.dispatch(startEditExpense(id, updates)).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        })
+        return database.ref(`expenses/${id}`).once('value')
+    }).then((snapshot) => {
+        expect(snapshot.val().amount).toBe(updates.amount)
+        done()
     })
 })
 
