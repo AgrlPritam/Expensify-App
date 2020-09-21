0dd07a1f9988b89322fc4d7e87b6b3ba5222b369
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-09-21 13:41:57 +0530

    Setting up google authentication and login testing

---
+++ src/actions/auth.js
@@ -0,0 +1,6 @@
+import { firebase, googleAuthProvider } from '../firebase/firebase'
+export const startLogin = () => {
+    return (dispatch) => {
+        return firebase.auth().signInWithPopup(googleAuthProvider)
+    }
+}
