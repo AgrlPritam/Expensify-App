ff704f3bf89b9d1db69fe7b324cf6a60927c761f
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-09-21 11:04:53 +0530

    Adding Remove Expense from database handler

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -50,6 +50,14 @@ export const removeExpense = ({ id } = {}) => ({
     id
 })
 
+export const startRemoveExpense = ({ id } = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({id}))
+        })
+    }
+}
+
 //Edit_Expense
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
@@ -79,4 +87,4 @@ export const startSetExpenses = () => {
             dispatch(setExpenses(expenses))
         })
     }
-} 
+}
