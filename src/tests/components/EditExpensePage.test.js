12c4401fb77564ab8e70d9615e024934f47305ec
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-10-04 13:42:34 +0530

    Modifying test case for remove expense

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -3,7 +3,7 @@ import {shallow} from 'enzyme'
 import {EditExpensePage} from '../../components/EditExpensePage'
 import {expenses} from '../fixtures/expenses'
 
-let startEditExpense,startRemoveExpense, history, wrapper;
+let startEditExpense, startRemoveExpense, history, wrapper;
 
 beforeEach(() => {
     startEditExpense = jest.fn()
@@ -29,7 +29,7 @@ test('Should handle startEditExpense',() => {
 })
 
 test('Should handle startRemoveExpense',() => {
-    wrapper.find('button').simulate('click')
+    wrapper.find('ExpenseRemoveModal').prop('onRemoveConfirm')(expenses[2])
     expect(history.push).toHaveBeenLastCalledWith('/')
     expect(startRemoveExpense).toHaveBeenLastCalledWith({
         id: expenses[2].id
