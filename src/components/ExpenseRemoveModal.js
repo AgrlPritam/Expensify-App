315489035ce7160b04046fa05679f29640516b97
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-10-04 14:10:17 +0530

    Addressing few errors

--- src/components/ExpenseRemoveModal.js
+++ src/components/ExpenseRemoveModal.js
@@ -4,15 +4,15 @@ import Modal from 'react-modal'
 const ExpenseRemoveModal = (props) => (
     <Modal
         isOpen = {!!props.promptRemove}
-        onRequestClose={handleClearPromptRemoveExpense}
+        onRequestClose={props.handleClearPromptRemoveExpense}
         contentLabel="Are you sure you want to remove this expense?"
         closeTimeoutMS={200}
         className="modal"
-        appElement="app"
+        appElement={app}
     >
         <h3 className="modal__title">Are you sure you want to remove this expense?</h3>
         <div className="input-group modal-input">
-            <button className="button button--delete input-group__item">Remove</button>
+            <button className="button button--delete input-group__item" onClick={props.onRemoveConfirm}>Remove</button>
             <button className="button button--secondary input-group__item" onClick={props.handleClearPromptRemoveExpense}>Cancel</button>
         </div>
     </Modal>
